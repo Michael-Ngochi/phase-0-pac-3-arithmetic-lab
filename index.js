588f4070b762df1a22802ccc1726233a3f36a9aa
Michael-Ngochi <ngoochimike@gmail.com>
2025-03-07 12:19:42 +0300

    added arithmetic funtions

---
+++ index.js
@@ -0,0 +1,17 @@
+let add =(a, b) => {
+    return a+b}
+    ;
+subtract=(a,b)=>a-b;
+multiply=(a,b)=>a*b;
+divide=(a,b)=>a/b;
+
+increment=(n)=>n+1;
+decrement=(n)=>n-1;
+
+makeInt=(n)=>{
+    return parseInt(n,10);
+}
+
+preserveDecimal=(n)=>{
+return parseFloat(n);
+}
